0b7d83a070f0d28da82d9f2da05b6a4e8fc52ef6
Tuhina Chanda <tuhinachanda44@gmail.com>
2024-09-08 10:02:12 +0530

    first commit

--- app.js
+++ app.js
@@ -20,9 +20,8 @@ const listingRouter=require("./routes/listing.js");
 const reviewRouter=require("./routes/review.js");
 const userRouter=require("./routes/user.js");
 
-/*const MONGO_URL="mongodb://127.0.0.1:27017/wanderlust";*/
+const MONGO_URL="mongodb://127.0.0.1:27017/wanderlust";
 
-const dbUrl=process.env.ATLASDB_URL;
 
 main()
 .then(()=>{
@@ -33,7 +32,7 @@ main()
 });
 
 async function main(){
-    await mongoose.connect(dbUrl);
+    await mongoose.connect(MONGO_URL);
 }
 
 app.set("view engine", "ejs");
@@ -43,8 +42,8 @@ app.use(methodOverride("_method"));
 app.engine('ejs', ejsMate);
 app.use(express.static(path.join(__dirname, "/public")));
 
-const store=MongoStore.create({
-    mongoUrl: dbUrl,
+/*const store=MongoStore.create({
+    mongoUrl,
     crypto: {
         secret: process.env.SECRET,
     },
@@ -53,10 +52,10 @@ const store=MongoStore.create({
 
 store.on("error", ()=>{
     console.log("ERROR in MONGO SESSION STORE", err);
-});
+});*/
 
 const sessionOptions={
-    store,
+    /*store,*/
     secret: process.env.SECRET,
     resave: false,
     saveUninitialized: true,
